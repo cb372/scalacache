a12ca0e9afaa810cbba31d27cade520d87d4f727
Chris Birchall <chris.birchall@gmail.com>
2013-11-16 14:04:15 +0900

    Add coveralls plugin

--- project/Build.scala
+++ project/Build.scala
@@ -1,5 +1,6 @@
 import sbt._
 import Keys._
+import CoverallsPlugin.CoverallsKeys._
 
 object CacheableBuild extends Build {
   
@@ -7,41 +8,42 @@ object CacheableBuild extends Build {
     val scala = "2.10.3"
   }
 
-  lazy val root = Project(
-    id = "cacheable",
-    base = file("."),
-    settings = standardSettings) aggregate(core, guava, memcached)
+  lazy val root = Project(id = "cacheable",base = file("."))
+    .settings(standardSettings: _*)
+    .settings(ScctPlugin.mergeReportSettings: _*)
+    .settings(CoverallsPlugin.multiProject: _*)
+    .settings(coverallsTokenFile := "coveralls-token.txt")
+    .aggregate(core, guava, memcached)
 
-  lazy val core = Project(
-    id = "cacheable-core",
-    base = file("core"),
-    settings = standardSettings ++ Seq(
+  lazy val core = Project(id = "cacheable-core", base = file("core"))
+    .settings(standardSettings: _*)
+    .settings(ScctPlugin.instrumentSettings: _*) 
+    .settings(
       libraryDependencies ++= Seq(
         "org.scala-lang" % "scala-reflect" % Versions.scala
       )
     )
-  )
 
-  lazy val guava = Project(
-    id = "cacheable-guava",
-    base = file("guava"),
-    settings = standardSettings ++ Seq(
+  lazy val guava = Project(id = "cacheable-guava", base = file("guava"))
+    .settings(standardSettings: _*)
+    .settings(ScctPlugin.instrumentSettings: _*) 
+    .settings(
       libraryDependencies ++= jodaTime ++ Seq(
         "com.google.guava" % "guava" % "15.0",
         "com.google.code.findbugs" % "jsr305" % "1.3.9"
       )
     )
-  ) dependsOn(core)
+    .dependsOn(core)
 
-  lazy val memcached = Project(
-    id = "cacheable-memcached",
-    base = file("memcached"),
-    settings = standardSettings ++ Seq(
+  lazy val memcached = Project(id = "cacheable-memcached", base = file("memcached"))
+    .settings(standardSettings: _*)
+    .settings(ScctPlugin.instrumentSettings: _*) 
+    .settings(
       libraryDependencies ++= jodaTime ++ Seq(
         "net.spy" % "spymemcached" % "2.10.2"
       )
     )
-  ) dependsOn(core)
+    .dependsOn(core)
 
   lazy val jodaTime = Seq(
     "joda-time" % "joda-time" % "2.3",
