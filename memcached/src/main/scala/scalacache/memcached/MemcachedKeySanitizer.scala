71d2e1cbd6053ebc873de049e8f35500d1952267
Lloyd <lloydmeta@gmail.com>
2014-08-30 23:02:53 +0900

    Docs state that after digest is called, the digest is reset anyways

--- memcached/src/main/scala/scalacache/memcached/MemcachedKeySanitizer.scala
+++ memcached/src/main/scala/scalacache/memcached/MemcachedKeySanitizer.scala
@@ -73,10 +73,5 @@ case class HashingMemcachedKeySanitizer(algorithm: HashingAlgorithm = MD5) exten
    * Uses the specified hashing algorithm to digest a key and spit out a hexidecimal representation
    * of the hashed key
    */
-  def toValidMemcachedKey(key: String): String = {
-    val digester = algorithm.messageDigest
-    val hash = digester.digest(key.getBytes).map("%02x".format(_)).mkString
-    digester.reset()
-    hash
-  }
+  def toValidMemcachedKey(key: String): String = algorithm.messageDigest.digest(key.getBytes).map("%02x".format(_)).mkString
 }
