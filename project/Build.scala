62df26da1c64680222096b2a4cfca8201b3c282b
Chris Birchall <chris.birchall@gmail.com>
2014-10-19 21:38:48 +0900

    0.4.2-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object ScalaCacheBuild extends Build {
   
   object Versions {
     val scala = "2.11.2"
-    val project = "0.4.1"
+    val project = "0.4.2-SNAPSHOT"
   }
 
   lazy val root = Project(id = "scalacache",base = file("."))
