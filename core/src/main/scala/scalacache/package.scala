5084e415be465ac2afb76429d77270d34e1e046d
KASHIMA, Kazuo <k4200@kazu.tv>
2016-11-07 23:31:47 +0900

    Wait for putWithKey to finish when flags.invalidateEnabled = true

--- core/src/main/scala/scalacache/package.scala
+++ core/src/main/scala/scalacache/package.scala
@@ -60,8 +60,12 @@ package object scalacache extends JavaSerializationCodec {
 
       def calculateAndCacheResult(): Future[From] = {
         val result: Future[From] = f
-        asynchronouslyCacheResult(result)
-        result
+        val cacheResult: Future[Unit] = asynchronouslyCacheResult(result)
+        if (flags.invalidateEnabled) {
+          result flatMap (v => cacheResult.map(_ => v))
+        } else {
+          result
+        }
       }
 
       val fromCache: Future[Option[From]] = getWithKey(key)
@@ -93,7 +97,7 @@ package object scalacache extends JavaSerializationCodec {
     }
 
     private def getWithKey(key: String)(implicit flags: Flags): Future[Option[From]] = {
-      if (flags.readsEnabled) {
+      if (flags.readsEnabled && !flags.invalidateEnabled) {
         scalaCache.cache.get[From](key)
       } else {
         if (logger.isDebugEnabled) {
@@ -104,7 +108,7 @@ package object scalacache extends JavaSerializationCodec {
     }
 
     private def putWithKey(key: String, value: From, ttl: Option[Duration] = None)(implicit flags: Flags): Future[Unit] = {
-      if (flags.writesEnabled) {
+      if (flags.writesEnabled || flags.invalidateEnabled) {
         val finiteTtl = ttl.filter(_.isFinite()) // discard Duration.Inf, Duration.Undefined
         scalaCache.cache.put(key, value, finiteTtl)
       } else {
