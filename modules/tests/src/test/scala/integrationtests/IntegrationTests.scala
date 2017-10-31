abaadef87b02586aca45e002709735a29a19f9e1
Chris Birchall <chris.birchall@gmail.com>
2017-10-31 09:47:18 +0000

    Write integration tests

--- modules/tests/src/test/scala/integrationtests/IntegrationTests.scala
+++ modules/tests/src/test/scala/integrationtests/IntegrationTests.scala
@@ -5,11 +5,13 @@ import java.util.UUID
 import org.scalatest._
 import cats.effect.{IO => CatsIO}
 import monix.eval.{Task => MonixTask}
-import monix.execution.Scheduler.Implicits.global
+import monix.execution.Scheduler
 
 import scalaz.concurrent.{Task => ScalazTask}
 import net.spy.memcached.{AddrUtil, MemcachedClient}
 
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 import scala.language.higherKinds
 import scalacache._
 import scalacache.caffeine.CaffeineCache
@@ -17,85 +19,107 @@ import scalacache.memcached.MemcachedCache
 
 class IntegrationTests extends FlatSpec with Matchers with BeforeAndAfterAll {
 
-//  trait Effect {
-//    type F[_]
-//    def name: String
-//    def mode: Mode[F]
-//    def unsafeRunSync[V](fv: F[V]): V
-//  }
-//
-//  private val catsEffectIO: Effect = new Effect {
-//    type F[A] = CatsIO[A]
-//    val name: String = "cats-effect IO"
-//    val mode: Mode[CatsIO] = scalacache.cats.effect.modes.io
-//    def unsafeRunSync[V](io: CatsIO[V]): V = io.unsafeRunSync()
-//  }
-//
-//  private val monixTask: Effect = new Effect {
-//    type F[A] = MonixTask[A]
-//    val name: String = "Monix Task"
-//    val mode: Mode[MonixTask] = scalacache.monix.modes.task
-//    def unsafeRunSync[V](task: MonixTask[V]): V = task.runSyncMaybe.right.get
-//  }
-//
-//  private val scalazTask: Effect = new Effect {
-//    type F[A] = ScalazTask[A]
-//    val name: String = "Scalaz Task"
-//    val mode: Mode[ScalazTask] = scalacache.scalaz72.modes.task
-//    def unsafeRunSync[V](task: ScalazTask[V]): V = task.unsafePerformSync
-//  }
-//
-//  private val memcachedClient = new MemcachedClient(AddrUtil.getAddresses("localhost:11211"))
-//
-//  override def afterAll(): Unit = {
-//    memcachedClient.shutdown()
-//  }
-//
-//  def memcachedIsRunning: Boolean = {
-//    try {
-//      memcachedClient.get("foo")
-//      true
-//    } catch { case _: Exception => false }
-//  }
-//
-//  case class CacheBackend(name: String, cache: Cache[String])
-//
-//  private val caffeine = CacheBackend("caffeine", CaffeineCache[String])
-//  private val memcached: Option[CacheBackend] =
-//    if (memcachedIsRunning)
-//      Some(CacheBackend("memcached", MemcachedCache[String](memcachedClient)))
-//    else
-//      None
-//
-//  val backends: List[CacheBackend] = List(Some(caffeine), memcached).flatten
-//  val effects: List[Effect] = List(catsEffectIO, monixTask, scalazTask)
-//
-//  for {
-//    CacheBackend(name, cache) <- backends
-//    effect <- effects
-//  } {
-//
-//    behavior of s"($name) ⇔ (${effect.name})"
-//
-//    it should "defer the computation" in {
-//      implicit val theCache: Cache[String] = cache
-//      implicit val mode: Mode[effect.F] = effect.mode
-//
-//      val key = UUID.randomUUID().toString
-//      val initialValue = UUID.randomUUID().toString
-//
-//      val program =
-//        for {
-//          _ <- put(key)(initialValue)
-//          readFromCache <- get(key)
-//          updatedValue = "prepended " + readFromCache.getOrElse("couldn't find in cache!")
-//          _ <- put(key)(updatedValue)
-//          finalValueFromCache <- get(key)
-//        } yield finalValueFromCache
-//
-//      assert(finalValueFromCache == "prepended " + initialValue)
-//    }
-//
-//  }
+  private val memcachedClient = new MemcachedClient(AddrUtil.getAddresses("localhost:11211"))
+
+  override def afterAll(): Unit = {
+    memcachedClient.shutdown()
+  }
+
+  def memcachedIsRunning: Boolean = {
+    try {
+      memcachedClient.get("foo")
+      true
+    } catch { case _: Exception => false }
+  }
+
+  case class CacheBackend(name: String, cache: Cache[String])
+
+  private val caffeine = CacheBackend("caffeine", CaffeineCache[String])
+  private val memcached: Option[CacheBackend] =
+    if (memcachedIsRunning)
+      Some(CacheBackend("memcached", MemcachedCache[String](memcachedClient)))
+    else
+      None
+
+  val backends: List[CacheBackend] = List(Some(caffeine), memcached).flatten
+
+  for (CacheBackend(name, cache) <- backends) {
+
+    s"($name) ⇔ (cats-effect IO)" should "defer the computation and give the correct result" in {
+      implicit val theCache: Cache[String] = cache
+      implicit val mode: Mode[CatsIO] = scalacache.cats.effect.modes.io
+
+      val key = UUID.randomUUID().toString
+      val initialValue = UUID.randomUUID().toString
+
+      import _root_.cats.syntax.all._
+      val program =
+        for {
+          _ <- put(key)(initialValue)
+          readFromCache <- get(key)
+          updatedValue = "prepended " + readFromCache.getOrElse("couldn't find in cache!")
+          _ <- put(key)(updatedValue)
+          finalValueFromCache <- get(key)
+        } yield finalValueFromCache
+
+      checkComputationHasNotRun(key)
+
+      val result: Option[String] = program.unsafeRunSync()
+      assert(result.contains("prepended " + initialValue))
+    }
+
+    s"($name) ⇔ (Monix Task)" should "defer the computation and give the correct result" in {
+      implicit val theCache: Cache[String] = cache
+      implicit val mode: Mode[MonixTask] = scalacache.monix.modes.task
+
+      val key = UUID.randomUUID().toString
+      val initialValue = UUID.randomUUID().toString
+
+      val program =
+        for {
+          _ <- put(key)(initialValue)
+          readFromCache <- get(key)
+          updatedValue = "prepended " + readFromCache.getOrElse("couldn't find in cache!")
+          _ <- put(key)(updatedValue)
+          finalValueFromCache <- get(key)
+        } yield finalValueFromCache
+
+      checkComputationHasNotRun(key)
+
+      val scheduler: Scheduler = _root_.monix.execution.Scheduler.global
+      val future = program.runAsync(scheduler)
+      val result = Await.result(future, Duration.Inf)
+      assert(result.contains("prepended " + initialValue))
+    }
+
+    s"($name) ⇔ (Scalaz Task)" should "defer the computation and give the correct result" in {
+      implicit val theCache: Cache[String] = cache
+      implicit val mode: Mode[ScalazTask] = scalacache.scalaz72.modes.task
+
+      val key = UUID.randomUUID().toString
+      val initialValue = UUID.randomUUID().toString
+
+      val program =
+        for {
+          _ <- put(key)(initialValue)
+          readFromCache <- get(key)
+          updatedValue = "prepended " + readFromCache.getOrElse("couldn't find in cache!")
+          _ <- put(key)(updatedValue)
+          finalValueFromCache <- get(key)
+        } yield finalValueFromCache
+
+      checkComputationHasNotRun(key)
+
+      val result: Option[String] = program.unsafePerformSync
+      assert(result.contains("prepended " + initialValue))
+    }
+
+  }
+
+  private def checkComputationHasNotRun(key: String)(implicit cache: Cache[String]): Unit = {
+    Thread.sleep(1000)
+    implicit val mode: Mode[Id] = scalacache.modes.sync.mode
+    assert(scalacache.sync.get(key).isEmpty)
+  }
 
 }
