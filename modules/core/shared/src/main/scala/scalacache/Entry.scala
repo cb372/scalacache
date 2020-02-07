30dafe52ed6026b3aeba3c1c73c45204621acafb
Jakub Kozłowski <kubukoz@gmail.com>
2020-02-07 02:29:33 +0100

    Port caffeine module

--- modules/core/shared/src/main/scala/scalacache/Entry.scala
+++ modules/core/shared/src/main/scala/scalacache/Entry.scala
@@ -1,16 +1,20 @@
 package scalacache
 
-import java.time.{Clock, Instant}
+import java.time.{Instant}
+import cats.effect.Clock
+import java.util.concurrent.TimeUnit
+import cats.implicits._
+import language.higherKinds
+import cats.Functor
 
 /**
   * A cache entry with an optional expiry time
   */
-case class Entry[+A](value: A, expiresAt: Option[Instant]) {
+case class Entry[F[_], +A](value: A, expiresAt: Option[Instant]) {
 
   /**
     * Has the entry expired yet?
     */
-  def isExpired(implicit clock: Clock): Boolean =
-    expiresAt.exists(_.isBefore(Instant.now(clock)))
-
+  def isExpired(implicit clock: Clock[F], functor: Functor[F]): F[Boolean] =
+    clock.monotonic(TimeUnit.MILLISECONDS).map(Instant.ofEpochMilli(_)).map(now => expiresAt.exists(_.isBefore(now)))
 }
