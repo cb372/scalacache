7f37ff5c8fdb27f9a890b7b3f73f1325279488c7
Krzysztof Nirski <krzysztof.nirski+github@gmail.com>
2022-11-29 21:28:17 +0100

    Add type annotations to new private defs in AbstractCache

--- modules/core/src/main/scala/scalacache/AbstractCache.scala
+++ modules/core/src/main/scala/scalacache/AbstractCache.scala
@@ -121,14 +121,14 @@ trait AbstractCache[F[_], K, V] extends Cache[F, K, V] with LoggingSupport[F, K]
         }
     }
 
-  private def read(key: K)(implicit flags: Flags) =
+  private def read(key: K)(implicit flags: Flags): F[Option[V]] =
     checkFlagsAndGet(key).handleErrorWith { e =>
       logger
         .ifWarnEnabled(logger.warn(s"Failed to read from cache. Key = $key", e))
         .as(None)
     }
 
-  private def write(key: K, value: V, ttl: Option[Duration])(implicit flags: Flags) =
+  private def write(key: K, value: V, ttl: Option[Duration])(implicit flags: Flags): F[Unit] =
     checkFlagsAndPut(key, value, ttl).handleErrorWith { e =>
       logger.ifWarnEnabled {
         logger.warn(s"Failed to write to cache. Key = $key", e)
